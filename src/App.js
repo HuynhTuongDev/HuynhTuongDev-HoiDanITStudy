38ac528f782b9fba5118c3e8ff21c31de818521f
Finn <lehuynhtuong13@gmail.com>
2025-03-06 16:44:32 +0700

    Bai 47

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className="app-container">
-      Hello World!
+      <Header />
     </div>
   );
 }
