ff93ffef3eab39a7741193000b229eeacd6c9fdd
Finn <lehuynhtuong13@gmail.com>
2025-03-13 19:16:00 +0700

    Bai 65

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -9,10 +9,20 @@ const postCreateNewUser = (email, password, username, role, image) => {
     data.append('userImage', image);
     return axios.post('api/v1/participant', data);
 }
+const putUpdateUser = (id, username, role, image) => {
+    //submit data
+    const data = new FormData();
+    data.append('id', id);
+    data.append('username', username);
+    data.append('role', role);
+    data.append('userImage', image);
+    return axios.put('api/v1/participant', data);
+}
 const getAllUser = () => {
     return axios.get('api/v1/participant/all');
 }
 export {
     postCreateNewUser,
     getAllUser,
+    putUpdateUser,
 }   
