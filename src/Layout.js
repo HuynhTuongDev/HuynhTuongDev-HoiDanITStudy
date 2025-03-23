ed7cf3eb23e77e1086dd019ea8e8ef3a8382720f
Finn <lehuynhtuong13@gmail.com>
2025-03-23 16:49:53 +0700

    Bai 82

--- src/Layout.js
+++ src/Layout.js
@@ -10,7 +10,15 @@ import Register from "./components/Admin/Auth/Register";
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ListQuiz from "./components/User/ListQuiz";
+import DetailQuiz from "./components/User/DetailQuiz";
 
+const NotFound = () => {
+    return (
+        <div className="alert alert-danger container mt-3">
+            404.Not found data wwith your current URL
+        </div>
+    )
+}
 const Layout = (props) => {
     return (
         <>
@@ -19,12 +27,15 @@ const Layout = (props) => {
                     <Route index element={<HomePage />} />
                     <Route path="users" element={<ListQuiz />} />
                 </Route>
+                <Route path="/quiz/:id" element={<DetailQuiz />} />
+
                 <Route path="admins" element={<Admin />}>
                     <Route index element={<DashBoard />} />
                     <Route path="manage-users" element={<ManageUser />} />
                 </Route>
                 <Route path="login" element={<Login />} />
                 <Route path="register" element={<Register />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
             <ToastContainer
                 position="top-right"
